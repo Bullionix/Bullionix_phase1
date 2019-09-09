468bc3649ab8bcbf4983263b85f097bd69dda9cb
vexycats <spader89d@yahoo.com>
2019-09-09 00:06:17 -0700

    added minting to functions, and some tweaking

--- Phase_2/contracts/Bullionix721.sol
+++ Phase_2/contracts/Bullionix721.sol
@@ -11,7 +11,7 @@ import './IERC20.sol';
 
 
 
-contract BullionixGenerator is ERC721Enumerable, ERC721Metadata, Ownable{
+contract BullionixGenerator is ERC721Enumerable, ERC721MetadataMintable{
     
     modifier isActive{
     require(isOnline == true);
@@ -50,36 +50,16 @@ constructor() public ERC721Metadata(name, symbol){
 }
 
 
-/* @dev Events
-*-----Events-------*
-* New Series Made
-* Staked to token
-* Burned and withdrew
-* ----Errors--------*
-* Not proper amount
-* Not Approved
-* Already Staked
-**/
 event NewSeriesMade(string indexed url, uint256 indexed numberToMint);
 event Staked(address indexed _sender, uint256 indexed _amount, uint256 indexed tokenStaked);
 event Burned(address indexed _sender,  uint256 indexed _amount, uint256 indexed _tokenId);
 
 /*
 TODO:
-        - Need to add admin abilities to owner.sol
-        - Make Admin storage here and make admin only modifiers or use owner
+      
         - Allow for admin to make new data series
-        - Figure out data structure for each new series
         - Create asset to series
 
-        Data Structure: 
-        struct {
-                string url
-                uint numberInSeries
-                uint DGX cost
-                uint fee
-        }
-        -
 */
 /* 
 * @dev changes online status to disable contract, must be current owner
@@ -88,12 +68,14 @@ TODO:
  function toggleOnline() public onlyOwner {
          isOnline = !isOnline;
  }
-
-
- /* 
-* @dev Create a new series
-*
-**/
+   /**
+     * @dev Create a new series of NFTs.
+     * @param _url location of metadata on backend server. Will be tacked onto the end of set url using returnURL().
+     * @param numberToMint  The token number for this series. 10 would make 10 tokens avaliable 
+     * @param DGXcost The amount of DGX to send
+     * @param _fee Bullionix fee for generation
+     * @return A boolean that indicates if the operation was successful.
+     */
  function createNewSeries(string memory _url, uint256 numberToMint, uint256 DGXcost, uint256 _fee) public onlyOwner returns (bool _success){
       //takes input from admin to create a new nft series. Will have to define how many tokens to make, how much DGX they cost, and the url from s3.
       require(msg.sender == owner(), 'Only Owner'); //optional as onlyOwner Modifier is used 
@@ -104,6 +86,7 @@ TODO:
           seriesToTokenId[total.add(i)].DGXcost = DGXcost;
           seriesToTokenId[total.add(i)].fee = _fee;
       }
+    
    emit NewSeriesMade(_url, numberToMint);
    return true;
  }
@@ -115,10 +98,15 @@ TODO:
  function stake(uint256 _tokenToBuy) public payable isActive returns (bool){
       //takes input from admin to create a new nft series. Will have to define how many tokens to make, how much DGX they cost, and the url from s3.
       require(seriesToTokenId[_tokenToBuy].fee >= 0, "Doesn't Exist yet!");
-      uint256  tempValue = seriesToTokenId[_tokenToBuy].DGXcost.add(seriesToTokenId[_tokenToBuy].fee);
-      _transferFrom(msg.sender, tempValue);
+      uint256  totalCost = seriesToTokenId[_tokenToBuy].DGXcost.add(seriesToTokenId[_tokenToBuy].fee);
+      _transferFrom(msg.sender, totalCost);
+      
      
-   emit Staked(msg.sender, tempValue, _tokenToBuy);
+//TODO: 
+//Create/Mint token and send to buyer
+   string memory fullURL = returnURL(_tokenToBuy);
+   require(mintWithTokenURI(msg.sender, _tokenToBuy, fullURL));
+   emit Staked(msg.sender, totalCost, _tokenToBuy);
    return true;
  }
 
@@ -131,15 +119,7 @@ TODO:
 function viewYourTokens() public view  returns (uint256[] memory _yourTokens){
        return super._tokensOfOwner(msg.sender);
 }
-/*
-  * @dev returns the entire tokenURI 
-  * @return uint256 with the id of the token
-  */
-function returnURL(uint256 _tokenId) public view returns (string memory _URL){
-   require(_exists(_tokenId), "ERC721: approved query for nonexistent token");
-   string memory uri = seriesToTokenId[_tokenId].url;
-   return string(abi.encodePacked("https://bullionix.io/metadata", uri));
-}
+
 
 // Internals 
 /*
@@ -148,19 +128,29 @@ function returnURL(uint256 _tokenId) public view returns (string memory _URL){
   * @dev returns the entire tokenURI 
   * @return uint256 with the id of the token
   */
- function _transferFrom (address _owner, uint256 _amount)
-    internal
+  
+  /*
+  * @dev returns the entire tokenURI 
+  * @return uint256 with the id of the token
+  */
+function returnURL(uint256 _tokenId) internal view returns (string memory _URL){
+   require(_exists(_tokenId), "ERC721: approved query for nonexistent token");
+   string memory uri = seriesToTokenId[_tokenId].url;
+   return string(abi.encodePacked("https://bullionix.io/metadata", uri));
+}
+
+  /*
+  * @dev transfer ERC20 token to contract
+  * @return true or false if failed
+  */
+ function _transferFrom (address _owner, uint256 _amount)internal returns (bool)
+    
   {
     require(IERC20(DGXContract1).transferFrom(_owner, address(this), _amount));
+    return true;
   }
 
-  function _isAdmin(address _admin)
-    internal view returns (bool)
-  {
-      
-    // TODO: Implement better admin priviledge
-    return (owner() == _admin);
-  }
+
   //TODO
  /* function burn(){
       
